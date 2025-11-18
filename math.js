57a6cdd090d33f2696e884d319d5a1e92fe4dcd3
JbriAn2000 <saliring0531@gmail.com>
2025-11-18 13:13:49 +0800

    Added math.js basic arithmetic functions

--- math.js
+++ math.js
@@ -17,3 +17,32 @@ function quo(num1,num2){
     quo = num1 / num2;
     console.log(quo);
 }
+/*
+sum(2,3);
+diff(2,3);
+prod(2,3);
+quo(2,3);
+*/
+function add(num1,num2){
+    return num1 + num2;
+}
+
+
+function subtract(num1,num2){
+    return num1 - num2;
+}
+
+
+function multiply(num1,num2){
+    return num1 * num2;
+}
+
+
+function divide(num1,num2){
+    return num1 / num2;
+}
+
+console.log(add(2,3));
+console.log(subtract(2,3));
+console.log(multiply(2,3));
+console.log(divide(2,3));
