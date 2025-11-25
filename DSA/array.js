dffca8c62fc2eb6bb1b3315d6e614a57cb5e74f9
onahonchloe-byte <onahonchloea@gmail.com>
2025-11-25 14:01:47 +0800

    Added delete method for DSA/array.js

--- DSA/array.js
+++ DSA/array.js
@@ -24,6 +24,7 @@ class Array{
     insert(index, element){
         //Correcting Statement || Must be false
         if(index <0 || index > this.length){
+        if(index <0 || index >= this.length){
             console.log("Invalid Index");
             return;
         }
@@ -38,12 +39,31 @@ class Array{
         this.length++;
     }
 
+    delete(index){
+
+        //Correcting Statement || Must be false
+        if(index <0 || index >= this.length){
+            console.log("Invalid Index");
+            return;
+        }
+
+        //Shifting Condition
+        for(let i= index; i< this.length; i++){
+            this.data[i] = this.data[i+1];
+        }
+
+        //After Condition
+        delete this.data[this.length-1];
+        this.length--;
+
+    }
 }
 
 //
-
 arr1 = new Array([4,2,3]);
 
 arr1.traverse();
 arr1.insert(0,9);
+arr1.traverse();
+arr1.delete(1);
 arr1.traverse();
