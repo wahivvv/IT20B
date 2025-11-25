84818a81ad70606e014a6af4d29da21d7a64ac68
onahonchloe-byte <onahonchloea@gmail.com>
2025-11-25 14:06:27 +0800

    Added utility methods for DSA/stack.js

--- DSA/DSA/stack.js
+++ DSA/DSA/stack.js
@@ -21,12 +21,32 @@ class Stack{
     }
 
     
+    is_empty(){
+        return this.topIndex === -1;
+    }
+
+    size(){
+        return this.topIndex+1;
+    }
+
+    peek(){
+        //Correcting Statement
+        if(this.is_empty()) return null;
+        return this.data[this.topIndex];
+    }
+
 }
 
 //
 stack1 = new Stack();
 
+console.log(stack1.is_empty());
 stack1.push(4);
 stack1.push(2);
 stack1.push(3);
 stack1.traverse();
+stack1.push(5);
+stack1.push(8);
+console.log(stack1.is_empty());
+console.log(stack1.size());
+console.log("The top element is "+stack1.peek());
