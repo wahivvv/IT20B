d7305e0591be72e122b6fe873e86cc1ed4d39b6c
wahivvv <wahivwahiv4@gmail.com>
2025-12-02 08:59:58 +0800

    Added dequeue  method for DSA/queue

--- queue.js
+++ queue.js
@@ -17,20 +17,31 @@ class Queue{
         }
 
         console.log(output);
+        return output;
     }
 
     is_empty(){
         return this.frontIndex > this.rearIndex;
     }
-    
+
     front(){
-        //Correction Statement
+        
         if(this.is_empty()) return null;
         return this.data[this.frontIndex];
     }
 
     size(){
         return this.rearIndex - this.frontIndex +1;
+    }
+
+    dequeue(){
+        
+        if(this.is_empty()) return null;
+
+        const value = this.data[this.frontIndex];
+        delete this.data[this.frontIndex];
+        this.frontIndex++;
+        return value;
     }
 }
 
@@ -39,8 +50,10 @@ let queue1 = new Queue();
 queue1.enqueue(4);
 queue1.enqueue(2);
 queue1.enqueue(3);
-queue1.traverse();
-//queue1.traverse();W
+
 console.log(queue1.is_empty());
 console.log(queue1.front());
 console.log(queue1.size());
+console.log(queue1.traverse());
+console.log("The front value "+queue1.dequeue()+ " is dequeued");
+console.log(queue1.traverse());
