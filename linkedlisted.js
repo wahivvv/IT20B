ff3ad33c8f7f54bb47aeb23beb15f76508a032f6
wahivvv <wahivwahiv4@gmail.com>
2025-12-02 09:11:48 +0800

    Added insert at end method for DSA/linkedlist.js

--- linkedlisted.js
+++ linkedlisted.js
@@ -27,6 +27,22 @@ class LinkedList{
         }
         return output;
     }
+
+    insert_at_end(element){
+        const node = new Node(element);
+
+        if(!this.head){
+            this.head = node;
+            return;
+        }
+
+        let current = this.head;
+        while (current.next){
+            current = current.next;
+        }
+
+        current.next = node;
+    }
 }
 
 //
@@ -34,4 +50,7 @@ let list1 = new LinkedList();
 list1.insert_at_beginning(4);
 list1.insert_at_beginning(2);
 list1.insert_at_beginning(3);
+list1.insert_at_end(4);
+list1.insert_at_end(2);
+list1.insert_at_end(3);
 console.log(list1.traverse());
