6106ee056b42d512e8a5a8ab7510ec7cc5d981bb
wahivvv <wahivwahiv4@gmail.com>
2025-12-02 09:13:59 +0800

    added search method

--- linkedlisted.js
+++ linkedlisted.js
@@ -58,13 +58,26 @@ class LinkedList{
 
         console.log("Target not found");
     }
+
+    search(element){
+        let current = this.head;
+        while(current){
+            if(current.value===element) 
+            //return current;
+            return "Element found";
+            current = current.next;
+        }
+
+        return "Element not found";
+    }
 }
 
 //
 let list1 = new LinkedList();
 list1.insert_at_end(4);
-list1.insert_at_end(2);
 list1.insert_at_end(3);
 list1.insert_at_end(5);
 list1.insert_after(5,2);
 console.log(list1.traverse());
+console.log(list1.traverse());
+console.log(list1.search(4));
