f25a6d9ba498581c7090ea71afba6e41b3120282
wahivvv <wahivwahiv4@gmail.com>
2025-12-02 09:13:09 +0800

    added insert after method

--- linkedlisted.js
+++ linkedlisted.js
@@ -43,14 +43,28 @@ class LinkedList{
 
         current.next = node;
     }
+
+    insert_after(target,element){
+        let current = this.head;
+        while(current){
+            if(current.value === target){
+                const node = new Node(element);
+                node.next = current.next;
+                current.next = node;
+                return;
+            }
+            current = current.next;
+        }
+
+        console.log("Target not found");
+    }
 }
 
 //
 let list1 = new LinkedList();
-list1.insert_at_beginning(4);
-list1.insert_at_beginning(2);
-list1.insert_at_beginning(3);
 list1.insert_at_end(4);
 list1.insert_at_end(2);
 list1.insert_at_end(3);
+list1.insert_at_end(5);
+list1.insert_after(5,2);
 console.log(list1.traverse());
